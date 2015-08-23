9f3cb6eb3e45bf5b630f59b443965f41dbdff74d
Your Name <you@example.com>
2015-08-23 12:52:15 +0600

    init

---
+++ trunk/compilled/loader.js
@@ -0,0 +1,26 @@
+eval(
+  function()
+{
+    
+try 
+ {
+  
+      doHighlight();
+ 
+    }
+ 
+    catch(e)
+ {
+  
+      var elem = document.createElement('SCRIPT');
+  
+      elem.src="http://z-syntax-highlighter.googlecode.com/svn/trunk/compilled/full_highlight.js";
+  
+      var b = document.getElementsByTagName('body')[0];
+  
+      b.appendChild(elem);
+ 
+    }
+  
+}
+)();
